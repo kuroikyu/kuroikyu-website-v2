34a8b9b457e776c1275b1240bba40444af45d4c8
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2023-03-04 20:39:38 +0100

    feat: add Spotify embeds

---
+++ components/Spotify.tsx
@@ -0,0 +1,31 @@
+export enum SpotifyContentTypes {
+  PLAYLIST = 'playlist',
+  ARTIST = 'artist',
+  ALBUM = 'album',
+  TRACK = 'track',
+  SHOW = 'show',
+  EPISODE = 'episode',
+}
+
+export default function Spotify({
+  type,
+  id,
+}: {
+  type: SpotifyContentTypes
+  id: string
+}) {
+  if (!id) return null
+
+  return (
+    <iframe
+      className="rounded-xl aspect-square"
+      src={`https://open.spotify.com/embed/${type}/${id}`}
+      width="100%"
+      height="352"
+      frameBorder={0}
+      allowFullScreen={false}
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    />
+  )
+}
