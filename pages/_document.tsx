669fc2af8e475b2331329b5e12be8806ef2322a2
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2023-03-05 20:53:15 +0100

    feat: add site logo/title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,25 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const dotSize = 1
+  const dotSpace = 20
+  const dotBetween = dotSpace - dotSize
+  const bgColor = '#111112'
+  const dotColor = '#3f3f46'
+
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-white text-black">
+      <body
+        className="bg-black text-zinc-100"
+        style={{
+          background: `
+            linear-gradient(90deg, ${bgColor} ${dotBetween}px, transparent 1%) center 220% / ${dotSpace}px ${dotSpace}px,
+            linear-gradient(${bgColor} ${dotBetween}px, transparent 1%) center 220% / ${dotSpace}px ${dotSpace}px,
+            ${dotColor}
+            `,
+        }}
+      >
         <Main />
         <NextScript />
       </body>
