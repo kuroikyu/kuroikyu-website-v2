sanity-io/nextjs-blog-cms-sanity-v3

feat: add `sanity-plugin-iframe-pane`

schemas/settings/OpenGraphPreview.tsx
@@ -13,12 +13,12 @@ async function init(): Promise<SatoriOptions['fonts']> {
     globalThis.Intl = globalThis.Intl || {}
     //@ts-expect-error
     globalThis.Intl.Segmenter = await createIntlSegmenterPolyfill(
-      fetch(new URL('public/break_iterator.wasm', import.meta.url))
+      fetch(new URL('public/break_iterator.wasm', import.meta.url)),
     )
   }
 
   const fontData = await fetch(
-    new URL('public/Inter-Bold.woff', import.meta.url)
+    new URL('public/Inter-Bold.woff', import.meta.url),
   ).then((res) => res.arrayBuffer())
 
   return [{ name: 'Inter', data: fontData, style: 'normal', weight: 700 }]
@@ -62,7 +62,7 @@ export default function OpenGraphPreview(props: Settings['ogImage']) {
         fonts,
       })
     },
-    { suspense: true }
+    { suspense: true },
   )
 
   return <OpenGraphSvg dangerouslySetInnerHTML={{ __html }} />

kuroikyu/kuroikyu-website-v2

(cherry picked from commit 00f509daa0d850137b0ad5941c23bbd10cc69ab4)

schemas/settings/OpenGraphPreview.tsx
@@ -13,12 +13,12 @@ async function init(): Promise<SatoriOptions['fonts']> {
 		globalThis.Intl = globalThis.Intl || {}
 		//@ts-expect-error
 		globalThis.Intl.Segmenter = await createIntlSegmenterPolyfill(
-			fetch(new URL('public/break_iterator.wasm', import.meta.url))
+			fetch(new URL('public/break_iterator.wasm', import.meta.url)),
 		)
 	}
 
 	const fontData = await fetch(
-		new URL('public/Catamaran-Bold.ttf', import.meta.url)
+		new URL('public/Catamaran-Bold.ttf', import.meta.url),
 	).then((res) => res.arrayBuffer())
 
 	return [{ name: 'Catamaran', data: fontData, style: 'normal', weight: 700 }]
@@ -62,7 +62,7 @@ export default function OpenGraphPreview(props: Settings['ogImage']) {
 				fonts,
 			})
 		},
-		{ suspense: true }
+		{ suspense: true },
 	)
 
 	return <OpenGraphSvg dangerouslySetInnerHTML={{ __html }} />