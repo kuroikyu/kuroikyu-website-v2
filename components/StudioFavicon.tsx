93c5080e78bf180edee4ae3036245b5ed256eed5
Kuroi Kyu <kuroikyu@users.noreply.github.com>
2024-03-03 10:58:18 +0100

    refactor: improve studio favicon handling

--- components/StudioFavicon.tsx
+++ components/StudioFavicon.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-import favicon from 'public/favicon/studio-favicon-32x32.png'
 import { FC } from 'react'
 
 export const StudioFavicon: FC = () => (
-	<Image src={favicon} alt="Studio Favicon" />
+	<svg
+		width="112"
+		height="104"
+		viewBox="0 0 112 104"
+		xmlns="http://www.w3.org/2000/svg"
+		className="fill-black"
+	>
+		<rect width="112" height="104" className="fill-[#28DBA6]" />
+		<path d="M51.5061 70.82V94H9.53809V9.69803H51.5061V47.762H54.1901L59.4361 9.69803H102.136L87.2521 51.788L102.136 94H59.1921L54.1901 70.82H51.5061Z" />
+	</svg>
 )
