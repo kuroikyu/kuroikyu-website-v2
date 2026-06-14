d758595c57d897727a0513562cc62b323d46b60b
Kuroi Kyu <hello@kuroikyu.com>
2026-06-14 23:10:12 +0200

    refactor: cleanup template leftovers

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,17 +4,10 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Kuroi Kyu
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        Embark on a nerdy adventure with me as we explore the tech, games, and all things geeky.
       </h4>
     </section>
   );
