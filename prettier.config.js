b7ee411b6f2c831bb64e63ee4800983a52594c95
Kuroi Kyu <hello@kuroikyu.com>
2026-06-19 18:48:57 +0200

    chore: update code rules

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
+	arrowParens: 'always',
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	// printWidth: 100,
+	endOfLine: 'lf',
+	plugins: [
+		// WARN: tailwind plugin should always be the last to load
+		'prettier-plugin-tailwindcss',
+	],
+}
+
+export default config
